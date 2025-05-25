11f91a2f24cbb13c92663d8d488590d7a4670bbf
anyel-ec <paulangel826@gmail.com>
2025-05-25 16:07:06 -0500

    chore: start project

---
+++ src/test/java/top/anyel/circuitbreaker/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
